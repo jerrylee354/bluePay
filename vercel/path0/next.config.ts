cf2321853c4e3cd1b20c96d3ba7d8e08e4357aec
Jerry lee <ab1f642d@gmail.com>
2025-07-21 10:56:28 +0000

    Creating an optimized production build ... > [PWA] Compile server > [PWA

---
+++ vercel/path0/next.config.ts
@@ -0,0 +1,33 @@
+
+import type {NextConfig} from 'next';
+import withPWAInit from 'next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default withPWA(nextConfig);
