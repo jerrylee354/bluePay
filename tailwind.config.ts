ea6eb2ad4f75b7346687d2ef72104190424f9b01
Jerry lee <ab1f642d@gmail.com>
2025-07-21 08:47:01 +0000

    但是圓角還是不正常根本不是圓角，所以把直角移除

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -57,9 +57,10 @@ export default {
         },
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        xl: '0.75rem',
+        lg: '0.5rem',
+        md: '0.375rem',
+        sm: '0.25rem',
       },
       keyframes: {
         'accordion-down': {
