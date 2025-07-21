7e83c4def342cef05bfc77fa41e35fc7d23eacb2
Jerry lee <ab1f642d@gmail.com>
2025-07-21 12:36:56 +0000

    重新更新一下SDK // Import the functions you need from the SDKs you need import

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, updateProfile } from "firebase/auth";
@@ -5,12 +6,12 @@ import { getFirestore, updateDoc } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyAu9OYsRVms6Dlrdermq5PLh2kMWeDrPNU",
+  authDomain: "bluepay-fojq8.firebaseapp.com",
+  projectId: "bluepay-fojq8",
+  storageBucket: "bluepay-fojq8.appspot.com",
+  messagingSenderId: "623887467932",
+  appId: "1:623887467932:web:9f5a3d57efd0a295907459"
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
