e9d86b5f03a309a8c7492046e8f3105c34ae7d65
Jerry lee <ab1f642d@gmail.com>
2025-07-22 04:12:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/dictionaries.ts
+++ src/dictionaries.ts
@@ -1,10 +1,14 @@
 import type { Locale } from '../i18n'
+import { i18n } from '../i18n'
 
 const dictionaries = {
   en: () => import('./dictionaries/en.json').then((module) => module.default),
   'zh-TW': () => import('./dictionaries/zh-TW.json').then((module) => module.default),
 }
 
-export const getDictionary = async (locale: Locale) => dictionaries[locale]()
+export const getDictionary = async (locale: Locale) => {
+  const selectedLocale = i18n.locales.includes(locale) ? locale : i18n.defaultLocale;
+  return dictionaries[selectedLocale]();
+}
 
 export type Dictionary = Awaited<ReturnType<typeof getDictionary>>
